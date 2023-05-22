1f2cf2b66ce81de3162808501c177877e3ae0a17
Małgorzata Krupanek <66110734+GosiaKk5@users.noreply.github.com>
2023-05-22 22:44:44 +0200

    more waves

--- MyFirstGame/Menagers/WavesMenager.py
+++ MyFirstGame/Menagers/WavesMenager.py
@@ -11,8 +11,11 @@ class WavesMenager:
         wave4 = [0, 2, 1, 0, 1, 0, 1, 1]
         wave5 = [2, 1, 0, 2, 2, 1, 0, 1, 2, 0, 1]
         wave6 = [2, 1, 0, 2, 2, 1, 0, 1, 2, 2, 1, 2, 2, 2]
+        wave7 = [2, 2, 2, 2, 0, 2, 0, 2, 2, 1, 1, 2, 2, 2, 0, 0, 1]
+        wave8 = [2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 1, 2, 2, 2, 0, 0, 1, 2, 2, 0, 2, 2, 2, 2, 2]
+        wave9 = [0, 0, 0, 0, 0, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 1, 2, 2, 1, 1, 2, 2, 1, 2, 0, 0, 1, 2, 2, 0, 2, 2, 2, 2, 2]
 
-        self.waves = [wave0, wave1, wave2, wave3, wave4, wave5, wave6]
+        self.waves = [wave0, wave1, wave2, wave3, wave4, wave5, wave6, wave7, wave8, wave9]
 
     def get_new_wave(self):
         self.wave_index += 1
